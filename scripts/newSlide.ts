e2e0f804b082b1ab34591b179d69cec21050a3f4
ken7253 <ken7253@gmail.com>
2023-03-26 04:09:59 +0900

    ワークスペース作成時の動作を追加

--- scripts/newSlide.ts
+++ scripts/newSlide.ts
@@ -1,16 +1,43 @@
-import { exec } from "node:child_process";
-import inquirer from "inquirer";
+import path from "node:path";
+import { writeFile, mkdir, cp } from "node:fs/promises";
+import { execSync, exec } from "node:child_process";
+import dayjs from "../node_modules/dayjs/esm/index";
 
-const crateWorkspace = () => {
-  inquirer
-    .prompt({
-      name: "slideName",
-      message: "Enter slide name",
-      type: "input",
-    })
-    .then((answer) => {
-      exec(`npm init -w ${answer.slideName} -y`, (err) => console.log(err));
-    });
+const crateWorkspace = (name?: string) => {
+  const workspaceName = name ?? dayjs("YYYY-MM-DD").toString();
+  exec(`npm init -w ${workspaceName} -y`, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // コマンドの設定
+      [
+        ["dev", "slidev"],
+        ["build", "slidev build"],
+        ["export", "slidev export"],
+      ].forEach((scripts) => {
+        execSync(
+          `npm set-script -w ${workspaceName} ${scripts[0]} ${scripts[1]}`
+        );
+      });
+      // スライドファイルの作成
+      const templateFilePath = path.join(
+        process.cwd(),
+        "scripts",
+        "assets",
+        "slide.md"
+      );
+      const slideFilePath = path.join(process.cwd(), workspaceName, "slide.md");
+      cp(templateFilePath, slideFilePath).catch((err) => console.log(err));
+
+      const styleDir = path.join(process.cwd(), workspaceName, "styles");
+      mkdir(styleDir)
+        .then(() => {
+          writeFile(path.join(styleDir, "index.ts"), 'import "./mod.css"');
+          writeFile(path.join(styleDir, "mod.css"), "");
+        })
+        .catch((err) => console.log(err));
+    }
+  });
 };
 
 crateWorkspace();
