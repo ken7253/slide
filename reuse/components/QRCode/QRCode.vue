6de92427105aef5ad900c35d4daf5a3716a1b75e
ken7253 <ken7253@gmail.com>
2024-08-06 00:45:54 +0900

    QRコードの表示

--- reuse/components/QRCode/QRCode.vue
+++ reuse/components/QRCode/QRCode.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+import QRCode from "qrcode";
 const props = defineProps({
   text: String,
+});
+
+const canvas = ref<HTMLCanvasElement>();
+
+onMounted(() => {
+  if (!(canvas.value instanceof HTMLCanvasElement)) return;
+  QRCode.toCanvas(canvas.value, props.text ?? "");
 });
 </script>
 
 <template>
-  <div>{{ props.text }}</div>
+  <canvas ref="canvas"></canvas>
 </template>
 
 <style lang="css" scoped></style>
