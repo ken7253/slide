1589aabf60ea3730cd0084d3e22b0374074efa12
ken7253 <ken7253@gmail.com>
2024-08-06 03:07:00 +0900

    eslintを設定

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,52 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+const compat = new FlatCompat();
+
+export default tseslint.config(
   {
-    files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    name: 'eslint/recommended',
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.browser },
+    },
+    rules: eslint.configs.recommended.rules,
   },
-];
+
+  {
+    name: 'eslint/custom-rules',
+    rules: {
+      eqeqeq: ['error', 'always', { null: 'ignore' }],
+      'require-unicode-regexp': ['error'],
+      radix: ['error'],
+      'prefer-template': ['error'],
+      'no-plusplus': ['error'],
+      camelcase: ['error'],
+    },
+  },
+
+  // ESLint typescript
+  ...tseslint.configs.recommended,
+
+  // eslint vue
+  ...pluginVue.configs['flat/essential'],
+
+  {
+    name: 'storybook/plugin',
+    plugins: compat.extends('plugin:storybook/recommended')[0].plugins,
+  },
+
+  {
+    name: 'storybook/stories',
+    files: ['**/*.stories.ts'],
+    rules: compat.extends('plugin:storybook/recommended')[1].rules,
+  },
+  {
+    name: 'storybook/settings',
+    files: ['.storybook/**.ts'],
+    rules: compat.extends('plugin:storybook/recommended')[2].rules,
+  },
+);
