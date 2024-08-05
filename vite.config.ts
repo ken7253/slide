5c7545bd03de752bf738624c4eb84e100bbca444
ken7253 <ken7253@gmail.com>
2024-08-06 00:44:53 +0900

    コメントの追記

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+/**
+ * @see https://ja.vitejs.dev/config/
+ */
 export default defineConfig({
   plugins: [vue()],
 });
