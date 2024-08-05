45e8143b3c96950da8aea176bd93c6f80850d70b
ken7253 <ken7253@gmail.com>
2024-08-06 00:46:25 +0900

    URLの反映

--- reuse/components/QRCode/QRCode.vue
+++ reuse/components/QRCode/QRCode.vue
@@ -1,20 +1,59 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import QRCode from "qrcode";
-const props = defineProps({
-  text: String,
+
+export type Props = {
+  text: string;
+  removeParams?: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  text: "",
+  removeParams: true,
 });
 
 const canvas = ref<HTMLCanvasElement>();
 
+const removeParams = (text: string) => {
+  if (!URL.canParse(text)) {
+    return text;
+  }
+  const noParamsURL = text.split("?")[0];
+
+  return props.removeParams ? noParamsURL : props.text;
+};
+
+const link = computed(() =>
+  URL.canParse(props.text) ? removeParams(props.text) : undefined
+);
+
 onMounted(() => {
   if (!(canvas.value instanceof HTMLCanvasElement)) return;
-  QRCode.toCanvas(canvas.value, props.text ?? "");
+  QRCode.toCanvas(canvas.value, removeParams(props.text) ?? "");
 });
 </script>
 
 <template>
-  <canvas ref="canvas"></canvas>
+  <a class="outer" :href="link" target="_blank" rel="noreferrer">
+    <canvas class="canvas" ref="canvas"></canvas>
+  </a>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.outer {
+  display: block;
+  width: fit-content;
+  border: solid 3px;
+  border-radius: 8px;
+  overflow: hidden;
+  color: #333;
+
+  &[href]:hover {
+    opacity: 0.8;
+  }
+}
+
+.canvas {
+  display: block;
+}
+</style>
